e36a1731a281f006dd3d658781e8b9b79d120fde
Xaviercrow <ttblazin@gmail.com>
2025-04-15 09:17:09 -0400

    Update Drones.py

--- Drones.py
+++ Drones.py
@@ -1,5 +1,28 @@
-me = tello.Tello()
+import djitellopy
+import pygame
+import threading
+import time
+pygame.init()
+screen = pygame.display.set_mode((600,400))
+updown = 0
+leftright = 0
+forwardback = 0
+rotation = 0
+me = djitellopy.Tello()
 me.connect()
 me.takeoff()
-sleep()
-me.move_forward()
+def moving():
+    while True:
+        me.send_rc_control(leftright, forwardback, updown, rotation)
+movingthread = threading.Thread(target= moving)
+movingthread.start()
+while True:
+    for e in pygame.event.get():
+        if e.type == pygame.KEYDOWN:
+            if (e.key == pygame.K_SPACE):
+                me.land()
+                quit()
+            elif (e.key == pygame.K_a):
+                leftright = -25
+            elif(e.key ==pygame.K_d):
+                leftright = 25 
