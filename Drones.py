493cc1374b842590d8ce1ba04177aa51b1ec5678
Xaviercrow <ttblazin@gmail.com>
2025-04-15 09:53:06 -0400

    Update Drones.py

--- Drones.py
+++ Drones.py
@@ -8,6 +8,7 @@ updown = 0
 leftright = 0
 forwardback = 0
 rotation = 0
+takeoff = 0
 me = djitellopy.Tello()
 me.connect()
 me.takeoff()
@@ -22,7 +23,39 @@ while True:
             if (e.key == pygame.K_SPACE):
                 me.land()
                 quit()
+            if(e.key == pygame.K_t):
+                takeoff
             elif (e.key == pygame.K_a):
                 leftright = -25
             elif(e.key ==pygame.K_d):
                 leftright = 25 
+            elif(e.key == pygame.K_q):
+                updown = 60
+            elif(e.key == pygame.K_e):
+                updown = -60
+            elif(e.key == pygame.K_w):
+                forwardback = 25
+            elif(e.key == pygame.K_s):
+                forwardback = -25
+            elif(e.key == pygame.K_l):
+                rotation = 75
+            elif(e.key == pygame.K_BACKSPACE):
+                me.emergency
+        if e.type == pygame.KEYUP:
+             if (e.key == pygame.K_SPACE):
+                me.land()
+                quit()
+             elif (e.key == pygame.K_a):
+                leftright = 0
+             elif(e.key ==pygame.K_d):
+                leftright = 0
+             elif(e.key == pygame.K_q):
+                updown = 0
+             elif(e.key == pygame.K_e):
+                updown = 0
+             elif(e.key == pygame.K_w):
+                forwardback = 0
+             elif(e.key == pygame.K_s):
+                forwardback = 0
+             elif(e.key == pygame.K_l):
+                rotation = 0
