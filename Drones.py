52327fd3ab8b3dfebe3f3d66ebcd15f661688482
Peytosequi <000260303@student.baisd.net>
2025-04-16 09:56:47 -0400

    Camera?

--- Drones.py
+++ Drones.py
@@ -1,21 +1,38 @@
 import djitellopy
 import pygame
 import threading
+import cv2
+# Initializing
 pygame.init()
 screen = pygame.display.set_mode((600,400))
+# Movement
 updown = 0
 leftright = 0
 forwardback = 0
 rotation = 0
 takeoff = 0
+# Connect
 me = djitellopy.Tello()
 me.connect()
+me.streamon()
 me.takeoff()
+# Movement Function
 def moving():
     while True:
         me.send_rc_control(leftright, forwardback, updown, rotation)
+# Camera Function 
+def show_camera():
+    while True:
+        frame = me.get_frame_read().frame
+        img = cv2.resize(frame, (600, 400))
+        cv2.imshow("Tello Camera", img)
+        if cv2.waitKey(1) & 0xFF == ord('x'):
+            break
+    cv2.destroyAllWindows()
+# Start Threads
 movingthread = threading.Thread(target= moving)
 movingthread.start()
+# Main Loop
 while True:
     for e in pygame.event.get():
         if e.type == pygame.KEYDOWN:
