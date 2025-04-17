ad39522ae63820398c191f2af60f452099f3541c
Peytosequi <000260303@student.baisd.net>
2025-04-17 08:45:13 -0400

    Color Detection?

--- Drones.py
+++ Drones.py
@@ -28,6 +28,39 @@ def show_camera():
     while running:
         frame = me.get_frame_read().frame
         img = cv2.resize(frame, (600, 400))
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+# HSV Ranges
+        color_ranges = {
+            "Red": [((0, 120, 70), (10, 255, 255)), ((170, 120, 70), (180, 255, 255))],
+            "Orange": [((10, 100, 100), (25, 255, 255))],
+            "Yellow": [((25, 100, 100), (35, 255, 255))],
+            "Green": [((40, 70, 70), (80, 255, 255))],
+            "Blue": [((100, 150, 0), (140, 255, 255))],
+            "Purple": [((140, 100, 100), (160, 255, 255))]
+        }
+        for color, ranges in color_ranges.items():
+            mask = None
+            for lower, upper in ranges:
+                if mask is None:
+                    mask = cv2.inRange(hsv, lower, upper)
+                else:
+                    mask |= cv2.inRange(hsv, lower, upper)
+
+            contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            for cnt in contours:
+                area = cv2.contourArea(cnt)
+                if area > 500:  # Filter small areas
+                    x, y, w, h = cv2.boundingRect(cnt)
+                    color_bgr = {
+                        "Red": (0, 0, 255),
+                        "Orange": (0, 165, 255),
+                        "Yellow": (0, 255, 255),
+                        "Green": (0, 255, 0),
+                        "Blue": (255, 0, 0),
+                        "Purple": (255, 0, 255)
+                    }[color]
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), color_bgr, 2)
+                    cv2.putText(frame, f"{color} Object", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, color_bgr, 2)
         cv2.imshow("Tello Camera", img)
         if cv2.waitKey(1) & 0xFF == ord('x'):
             running = False
